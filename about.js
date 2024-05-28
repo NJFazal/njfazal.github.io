1dfd5be696b178f1b6d99ebebf17fb3405e5913b
Nicole Fazal <108958417+NJFazal@users.noreply.github.com>
2024-05-27 22:40:02 -0400

    Add files via upload

--- about.js
+++ about.js
@@ -3,13 +3,13 @@ function showAbout() {
         {
             name: " Hi, I'm Nicky!",
             imageUrl: "images/aboutme.png",
-            description: "I am a full stack web developer. I got my MERN stack certification from MIT xPRO. I have over 6 years of GIS experience but my favorite way to include topography in my life is to go snowboarding. I am passionate about games and puzzle solving, so I am always excited to learn new technologies and creative coding."
+            description: "I am a full stack web developer. I got my MERN stack certification from MIT xPRO. I have over 6 years of GIS experience but my favorite way to include topography in my life is to go snowboarding. I am passionate about games and puzzle solving, so I am always excited to learn new technologies and creative coding solutions."
         }
     ]
     let aboutHTML = "";
     aboutMe.forEach(aboutme => {
         aboutHTML += `
-        <div class="card" style="width: 40rem; border: none; overflow-y: auto; padding:10px; padding-right:90px">
+        <div class="card" style="background-color: bisque; width: 40rem; border: none; overflow-y: auto; padding:10px; padding-right:90px">
   <img  src="${aboutme.imageUrl}" style="width:40%; margin-left:auto; margin-right:auto;" alt="me">
   <div class="card-body" style="padding:0px">
     <h1 style="text-align: center; font-size: 50px" >${aboutme.name}</h1>
