dff0edc67456796b288d94c070db044017dc539a
Nicole Fazal <108958417+NJFazal@users.noreply.github.com>
2024-10-16 21:26:56 -0400

    Add files via upload

--- contact.js
+++ contact.js
@@ -12,14 +12,14 @@ function showContact() {
             <img src="${contactMethod.imageUrl}" style="width:40%; margin-left:auto; margin-right:auto;" alt="me">
             <div class="card-body">
                 <p class="card-text" style="text-align: center; font-size: 50px">
-                                 <a style="text-decoration: none; text-align: center; font-size: 50px" href="https://github.com/NJFazal">GitHub</a></br>
-                    <a style="text-decoration: none; text-align: center; font-size: 50px" href="https://www.linkedin.com/in/nicole-fazal">LinkedIn</a>
                 <form action="https://formsubmit.co/nicolejfazal@gmail.com" method="POST">
-                    <input type="email" name="email" placeholder="Email Address" required>
+                    <a style="text-decoration: none; text-align: center; font-size: 50px" href="https://github.com/NJFazal">GitHub</a></br>
+                    <a style="text-decoration: none; text-align: center; font-size: 50px" href="https://www.linkedin.com/in/nicole-fazal">LinkedIn</a><br>
+                    <input type="email" name="email" placeholder="Email Address" required><br>
                     <input type="hidden" name="_next" value="https://nicolefazal.netlify.app/thankyou.html">
                     <input type="hidden" name="_captcha" value="false">
-                    <input type="text" name="message" placeholder="Write your message here!" required>
-                    <button type="submit">Send</button>
+                    <input style="height: 100px" type="text" name="message" placeholder="Send me a message!" required> <br>
+                    <button style="border-radius: 5px; font-size:30px; background-color: pink" type="submit">Send</button>
                 </form>
                 </p>
             </div>
