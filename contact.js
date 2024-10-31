4ce67b247b4620bef40ad3fb6021099b0cb5d08e
Nicole Fazal <108958417+NJFazal@users.noreply.github.com>
2024-10-30 21:19:31 -0400

    Add files via upload

--- contact.js
+++ contact.js
@@ -14,13 +14,13 @@ function showContact() {
                 <p class="card-text" style="text-align: center; font-size: 50px">
                 <form action="https://formsubmit.co/nicolejfazal@gmail.com" method="POST">
                 <input type="hidden" name="_template" value="box">
-                    <a style="text-decoration: none; text-align: center; font-size: 50px" href="https://github.com/NJFazal">GitHub</a></br>
+                    <a style="text-decoration: none; text-align: center; font-size: 50px;" href="https://github.com/NJFazal">GitHub</a></br>
                     <a style="text-decoration: none; text-align: center; font-size: 50px" href="https://www.linkedin.com/in/nicole-fazal">LinkedIn</a><br>
                     <input type="email" name="email" placeholder="Email Address" required><br>
                     <input type="hidden" name="_next" value="https://nicolefazal.netlify.app/thankyou.html">
                     <input type="hidden" name="_captcha" value="false">
                     <input style="height: 100px" type="text" name="message" placeholder="Send me a message!" required> <br>
-                    <button style="border-radius: 5px; font-size:30px; background-color: pink" type="submit">Send</button>
+                    <button style="border-radius: 5px; font-size:30px; background-color: #E7717D;" type="submit">Send</button>
                 </form>
                 </p>
             </div>
